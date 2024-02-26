ac7cf24493cfd4cbde1abd25c7bb287458842c97
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-26 12:06:47 +0100

    Begginnig of program

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace BankoCheater
+﻿// BankoCheater
+int[,] plate = new int[3, 5];
+string inputText = "";
+MakeBankoPlates("Hej", 1);
+PrintPlates();
+void MakeBankoPlates(string plateID, int plateAmount)
 {
-    internal class Program
+    Console.WriteLine();
+    Console.WriteLine("How many plates do you have?");
+    plateAmount = Convert.ToInt32(Console.ReadLine());
+
+    for (int k = 0; k < plateAmount; k++)
     {
-        static void Main(string[] args)
+        Console.WriteLine($"Type in your {plateID} plate, row 1. Separate the numbers with ','");
+        inputText = Console.ReadLine().Trim();
+        string[] inputArray = inputText.Split(",");
+        for (int i = 0; i < inputArray.Length; i++)
         {
-            Console.WriteLine("Hello, World!");
+            plate[0, i] = Convert.ToInt32(inputArray[i]);
         }
+
+        Console.WriteLine($"Type in your {plateID} plate, row 2. Separate the numbers with ','");
+        inputText = Console.ReadLine().Trim();
+        inputArray = inputText.Split(",");
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            plate[1, i] = Convert.ToInt32(inputArray[i]);
+        }
+
+        Console.WriteLine($"Type in your {plateID} plate, row 3. Separate the numbers with ','");
+        inputText = Console.ReadLine().Trim();
+        inputArray = inputText.Split(",");
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            plate[2, i] = Convert.ToInt32(inputArray[i]);
+        }
+    }
+}
+
+//Console.WriteLine("Number drawn: ");
+//int numbersDrawn= Convert.ToInt32(Console.ReadLine());
+void PrintPlates()
+{
+    Console.WriteLine("Your plate: ");
+    for (int i = 0; i < plate.GetLength(1); i++)
+    {
+        Console.Write(plate[0, i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < plate.GetLength(1); i++)
+    {
+        Console.Write(plate[1, i] + " ");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < plate.GetLength(1); i++)
+    {
+        Console.Write(plate[2, i] + " ");
     }
 }
--- BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
+++ BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e32d3465e32dd9b200ec5af9debdaa450171d97")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
