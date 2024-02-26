0aec8a5d9f6b84e6eb372f58d168b2325e5619f3
StineDk <144903306+StineDk@users.noreply.github.com>
2024-02-26 14:20:41 +0100

    Banko on one row

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -1,10 +1,12 @@
 ﻿// BankoCheater
+using System.Reflection.Emit;
+
 int[,] plate1 = new int[3, 5];
 int[,] plate2 = new int[3, 5];
 int[,] plate3 = new int[3, 5];
 string inputText = "";
-
-PrintPlates();
+bool check = false;
+bool play = true;
 
 Console.WriteLine();
 Console.WriteLine("What is your plateID?");
@@ -34,11 +36,37 @@ for (int i = 0; i < inputArray.Length; i++)
     plate1[2, i] = Convert.ToInt32(inputArray[i]);
 }
 
-//Console.WriteLine("Number drawn: ");
-//int numbersDrawn= Convert.ToInt32(Console.ReadLine());
+Label:
+Console.WriteLine("Number Drawn: ");
+int numberDrawn = Convert.ToInt32(Console.ReadLine());
+
+if (numberDrawn != null)
+{
+    check = true;
+}
+while (check == true)
+{
+    for (int i = 0; i < plate1.GetLength(0); i++)
+    {
+        for (int j = 0; j < plate1.GetLength(1); j++)
+        {
+            if (plate1[i, j] == numberDrawn)
+            {
+                plate1[i, j] = 0;
+                
+                if (plate1[i,0] == 0 && plate1[i,1] == 0 && plate1[i,2] == 0 && plate1[i,3] == 0 && plate1[i,4] == 0)
+                {
+                    Console.WriteLine($"You have Banko on {plateID} row {i+1}");
+                }
+            }
+        }
+    }
+    PrintPlates();
+    goto Label;
+}
 void PrintPlates()
 {
-    Console.WriteLine("Your plate: ");
+    Console.WriteLine($"Plate ID: {plateID}");
     for (int i = 0; i < plate1.GetLength(1); i++)
     {
         Console.Write(plate1[0, i] + " ");
@@ -53,4 +81,5 @@ void PrintPlates()
     {
         Console.Write(plate1[2, i] + " ");
     }
+    Console.WriteLine();
 }
